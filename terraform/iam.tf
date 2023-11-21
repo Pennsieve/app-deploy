71066cc80f2d6735f3d9b741e5581ad41b125a2f
Edmore Moyo <etmoyo@gmail.com>
2023-11-20 22:21:35 -0500

    cleans up templates

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -30,38 +30,6 @@ resource "aws_iam_policy" "lambda_iam_policy" {
   policy = data.aws_iam_policy_document.iam_policy_document_gateway.json
 }
 
-// Post processor lambda
-// allow lambda to access resources in your AWS account
-# data "aws_iam_policy_document" "post_processor_assume_role" {
-#   statement {
-#     effect = "Allow"
-
-#     principals {
-#       type        = "Service"
-#       identifiers = ["lambda.amazonaws.com"]
-#     }
-
-#     actions = ["sts:AssumeRole"]
-#   }
-# }
-
-# resource "aws_iam_role" "iam_for_post_processor_lambda" {
-#   name               = "iam_for_post_processor_lambda"
-#   assume_role_policy = data.aws_iam_policy_document.post_processor_assume_role.json
-# }
-
-# // attach policy to allow post-processor lambda to write to Cloudwatch
-# resource "aws_iam_role_policy_attachment" "lambda_policy_post_processor" {
-#   role       = aws_iam_role.iam_for_post_processor_lambda.name
-#   policy_arn = aws_iam_policy.post_processor_lambda_iam_policy.arn
-# }
-
-# resource "aws_iam_policy" "post_processor_lambda_iam_policy" {
-#   name   = "post-processor-lambda-iam-policy-${random_uuid.val.id}"
-#   path   = "/"
-#   policy = data.aws_iam_policy_document.iam_policy_document_post_processor.json
-# }
-
 // ## Main App ##
 // ECS task IAM role
 resource "aws_iam_role" "task_role_for_ecs_task" {
