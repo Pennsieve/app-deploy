4667865bebc9a04cb87d816ae12ae4b6809a3137
Edmore Moyo <etmoyo@gmail.com>
2023-12-11 12:05:36 -0500

    updates outputs

--- terraform/remote-state/outputs.tf
+++ terraform/remote-state/outputs.tf
@@ -1,5 +1,11 @@
 output "aws_bucket_name" {
-  description = "Bucket Name"
+  description = "State Bucket Name"
 
   value = aws_s3_bucket.terraform_state.bucket
+}
+
+output "dynamodb_table_name" {
+  description = "State Table Name"
+
+  value = aws_dynamodb_table.terraform_state_lock.name
 }
