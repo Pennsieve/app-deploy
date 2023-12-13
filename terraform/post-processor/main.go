f676245bf8685b522afb72c7aa2162041223eef1
Edmore Moyo <etmoyo@gmail.com>
2023-12-13 15:33:34 -0500

    updates for agent bucket in prod

--- terraform/post-processor/main.go
+++ terraform/post-processor/main.go
@@ -16,14 +16,22 @@ func main() {
 	environment := os.Getenv("ENVIRONMENT")
 	fmt.Println("ENVIRONMENT: ", environment)
 	fmt.Println("PENNSIEVE_API_HOST: ", os.Getenv("PENNSIEVE_API_HOST"))
+	fmt.Println("PENNSIEVE_UPLOAD_BUCKET: ", os.Getenv("PENNSIEVE_UPLOAD_BUCKET"))
 	if environment == "prod" {
-		fmt.Println("unsetting variable")
-		err := os.Unsetenv("PENNSIEVE_API_HOST")
+		fmt.Println("unsetting variables")
+		apiHostErr := os.Unsetenv("PENNSIEVE_API_HOST")
+		if apiHostErr != nil {
+			fmt.Println("error unsetting variable PENNSIEVE_API_HOST:",
+				apiHostErr)
+		}
+		err := os.Unsetenv("PENNSIEVE_UPLOAD_BUCKET")
 		if err != nil {
-			fmt.Println("error unsetting variable", err)
+			fmt.Println("error unsetting variable PENNSIEVE_UPLOAD_BUCKET:",
+				err)
 		}
 	}
 	fmt.Println("PENNSIEVE_API_HOST: ", os.Getenv("PENNSIEVE_API_HOST"))
+	fmt.Println("PENNSIEVE_UPLOAD_BUCKET: ", os.Getenv("PENNSIEVE_UPLOAD_BUCKET"))
 
 	cmd := exec.Command("/bin/sh", "./agent.sh", datasetID, integrationID)
 	out, err := cmd.Output()
