ab4c32c7a00ed5223e16e5b1aa9e1aa99211ca4a
Edmore Moyo <etmoyo@gmail.com>
2023-12-19 16:00:17 -0500

    refactors and adds sample files

--- scripts/infrastructure.sh
+++ scripts/infrastructure.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-cd $1
-export TF_DATA_DIR=$2
+DEPLOYMENTS_DIR="/service/application-deployments"
+export TF_DATA_DIR="${DEPLOYMENTS_DIR}/${ENVIRONMENT}/${GIT_REPOSITORY}"
+mkdir -p $TF_DATA_DIR
+
+TERRAFORM_DIR="/service/terraform"
+cd $TERRAFORM_DIR
+VAR_FILE="$TF_DATA_DIR/${ENVIRONMENT}.tfvars"
+BACKEND_FILE="$TF_DATA_DIR/${ENVIRONMENT}.tfbackend"
+PLAN_FILE="$TF_DATA_DIR/tfplan"
+SVG_FILE="$TF_DATA_DIR/graph.svg"
 
 echo "Creating backend config"
-  /bin/cat > "$2/${ENVIRONMENT}.tfbackend" <<EOL
+  /bin/cat > "$TF_DATA_DIR/${ENVIRONMENT}.tfbackend" <<EOL
 bucket  = "${TF_REMOTE_BUCKET}"
 key     = "${ENVIRONMENT}/${GIT_REPOSITORY}/terraform.tfstate"
 EOL
 
 echo "Creating tfvars config"
-  /bin/cat > "$2/${ENVIRONMENT}.tfvars" <<EOL
+  /bin/cat > "$TF_DATA_DIR/${ENVIRONMENT}.tfvars" <<EOL
 region = "${AWS_DEFAULT_REGION}"
 az = ["a", "b", "c", "d", "e", "f"]
 app_repository = "app"
@@ -25,21 +33,21 @@ api_key_secret = {
 environment = "${ENVIRONMENT}"
 EOL
 
-if [ $7 = "destroy" ]; then
+if [ $1 = "destroy" ]; then
   echo "Running destroy ..."
-  terraform apply -destroy -auto-approve -var-file=$5 > $2/destroy.log
+  terraform apply -destroy -auto-approve -var-file=$VAR_FILE > $TF_DATA_DIR/destroy.log
 else
   echo "Running init and plan ..."
-  terraform init -force-copy -backend-config=$3
-  terraform plan -out=$4 -var-file=$5
+  terraform init -force-copy -backend-config=$BACKEND_FILE
+  terraform plan -out=$PLAN_FILE -var-file=$VAR_FILE
 
   echo "Generating output ..."
   terraform output
   echo "Generating graph ..."
-  terraform graph -draw-cycles | dot -Tsvg > $6
+  terraform graph -draw-cycles | dot -Tsvg > $SVG_FILE
 
-  if [ $7 = "create" ]; then
+  if [ $1 = "create" ]; then
    echo "Running apply ..."
-   terraform apply $4 > $2/apply.log
+   terraform apply $PLAN_FILE > $TF_DATA_DIR/apply.log
   fi
 fi
