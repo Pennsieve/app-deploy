97ff177b0f4f2813c4c74b5f89c53c3a1120635b
Edmore Moyo <etmoyo@gmail.com>
2024-01-08 16:23:40 -0500

    defaults output directory

--- terraform/post-processor/agent.sh
+++ terraform/post-processor/agent.sh
@@ -5,7 +5,8 @@ pennsieve whoami
 pennsieve dataset use $1
 
 ls -alh /mnt/efs/output/$2
-pennsieve manifest create /mnt/efs/output/$2
+timestamp=$(date +%Y%m%d_%H%M%S%Z)
+pennsieve manifest create /mnt/efs/output/$2 -t "output-$timestamp-$2"
 pennsieve manifest list 1
 pennsieve upload manifest 1
 cat "/root/.pennsieve/agent.log"
