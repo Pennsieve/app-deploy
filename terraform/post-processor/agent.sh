6730d0f0f17e4ec9e1f85eb14f2ffbce0cea1a0d
Edmore Moyo <etmoyo@gmail.com>
2024-01-09 14:27:11 -0500

    removes print statement

--- terraform/post-processor/agent.sh
+++ terraform/post-processor/agent.sh
@@ -7,7 +7,6 @@ pennsieve dataset use $1
 ls -alh /mnt/efs/output/$2
 timestamp=$(date +%Y%m%d_%H%M%S%Z)
 target_path="${TARGET_PATH:-"output-$timestamp-$2"}"
-print $target_path
 pennsieve manifest create /mnt/efs/output/$2 -t $target_path
 pennsieve manifest list 1
 pennsieve upload manifest 1
