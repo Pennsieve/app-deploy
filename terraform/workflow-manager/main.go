2f8053ff4fe9d9110cf7d702f5147f344bab9eec
Edmore Moyo <etmoyo@gmail.com>
2024-01-26 15:11:06 -0500

    updates workflow manager

--- terraform/workflow-manager/main.go
+++ terraform/workflow-manager/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"bytes"
-	"io"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"log/slog"
-	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+const (
+	visibilityTimeout = 60 * 10
+	waitingTimeout    = 20
 )
 
 func main() {
@@ -17,43 +29,91 @@ func main() {
 	log.Println("Welcome to the WorkflowManager")
 	log.Println("Starting pipeline")
 
-	// // run pipeline
-	// cmd := exec.Command("nextflow", "run", "./workflows/pennsieve.nf", "-ansi-log", "false")
-	// cmd.Dir = "/service"
-	// var stdout strings.Builder
-	// var stderr strings.Builder
-	// cmd.Stdout = &stdout
-	// cmd.Stderr = &stderr
-	// if err := cmd.Run(); err != nil {
-	// 	logger.Error(err.Error(),
-	// 		slog.String("error", stderr.String()))
-	// }
-	// log.Println(stdout.String())
-	srv := &http.Server{
-		Addr:    ":8081",
-		Handler: NewHandler(),
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Fatalf("LoadDefaultConfig: %v\n", err)
 	}
 
-	log.Fatal(srv.ListenAndServe())
+	ctx, cancel := context.WithCancel(context.Background())
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	queueUrl := os.Getenv("SQS_URL")
+	log.Printf("QUEUE_URL: %s", queueUrl)
+
+	sqsSvc := sqs.NewFromConfig(cfg)
+
+loop:
+	for {
+		select {
+		case <-signalChan: //if get SIGTERM
+			log.Println("Got SIGTERM signal, cancelling the context")
+			cancel() //cancel context
+
+		default:
+			_, err := processSQS(ctx, sqsSvc, queueUrl)
+
+			if err != nil {
+				if errors.Is(err, context.Canceled) {
+					log.Printf("stop processing, context is cancelled %v", err)
+					break loop
+				}
+
+				log.Fatalf("error processing SQS %v", err)
+			}
+		}
+	}
+	log.Println("service is safely stopped")
+
 }
 
-func NewHandler() http.Handler {
-	mux := http.NewServeMux()
-	mux.Handle("/start", &WorkflowManagerServiceHandler{})
-	return mux
+type MsgType struct {
+	Message string `json:"message"`
 }
 
-func (dh *WorkflowManagerServiceHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	_ = req.Context()
-	body := req.Body
-	defer body.Close()
-	rw.WriteHeader(http.StatusAccepted)
-	rw.Header().Set("Content-Type", "application/json")
-	var b bytes.Buffer
-	io.Copy(&b, body)
-	log.Print(b.String())
-	rw.Write(b.Bytes())
-}
+func processSQS(ctx context.Context, sqsSvc *sqs.Client, queueUrl string, ddbSvc *dynamodb.Client, tableName string) (bool, error) {
+	input := &sqs.ReceiveMessageInput{
+		QueueUrl:            &queueUrl,
+		MaxNumberOfMessages: 1,
+		VisibilityTimeout:   visibilityTimeout,
+		WaitTimeSeconds:     waitingTimeout, // use long polling
+	}
 
-type WorkflowManagerServiceHandler struct {
+	resp, err := sqsSvc.ReceiveMessage(ctx, input)
+
+	if err != nil {
+		return false, fmt.Errorf("error receiving message %w", err)
+	}
+
+	log.Printf("received messages: %v", len(resp.Messages))
+	if len(resp.Messages) == 0 {
+		return false, nil
+	}
+
+	for _, msg := range resp.Messages {
+		var newMsg MsgType
+		id := *msg.MessageId
+
+		err := json.Unmarshal([]byte(*msg.Body), &newMsg)
+		if err != nil {
+			return false, fmt.Errorf("error unmarshalling %w", err)
+		}
+
+		log.Printf("message id %s is received from SQS: %#v", id, newMsg.Message)
+
+		// Do stuff with message
+
+		_, err = sqsSvc.DeleteMessage(ctx, &sqs.DeleteMessageInput{
+			QueueUrl:      &queueUrl,
+			ReceiptHandle: msg.ReceiptHandle,
+		})
+
+		if err != nil {
+			return false, fmt.Errorf("error deleting message from SQS %w", err)
+		}
+		log.Printf("message id %s is deleted from queue", id)
+
+	}
+	return true, nil
 }
