de35c7dbf7044ef3242d58dc43b240658a5dc370
Edmore Moyo <etmoyo@gmail.com>
2024-01-29 16:22:52 -0500

    removes unused queue

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,14 +1,4 @@
 // SQS queue for pipeline runs
-resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "queue-${random_uuid.val.id}.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-
-  tags = {
-    Environment = "${var.environment}"
-  }
-}
-
 resource "aws_sqs_queue" "pipeline_queue" {
   name                      = "queue-${random_uuid.val.id}"
 
