98d0d69d5f2c9575aa1dd3c60b62fca975d7dcde
Edmore Moyo <etmoyo@gmail.com>
2024-02-16 16:06:08 -0500

    fixes command

---
+++ terraform/application-wrapper/dependencies/autoHLA/R/header.R
@@ -0,0 +1,11 @@
+#' @import ggplot2
+#' @import dplyr
+#' @importFrom magrittr "%>%"
+#' @importFrom tidyr pivot_longer pivot_wider
+#' @importFrom readr read_csv write_csv
+#' @importFrom stringr str_split fixed str_replace str_remove str_replace_all
+#' @importFrom tibble tibble as_tibble
+#' @importFrom flowCore read.FCS compensate
+#' @importFrom KernSmooth bkde
+#' @importFrom ggridges geom_ridgeline
+NULL
