c390ea0214c5b0df7746771b613c9d367db4cf18
Edmore Moyo <etmoyo@gmail.com>
2024-02-26 18:25:24 -0500

    assumes role

--- terraform/compute-node-service/compute-node-service-lambda/compute-node-service.py
+++ terraform/compute-node-service/compute-node-service-lambda/compute-node-service.py
@@ -57,13 +57,14 @@ def lambda_handler(event, context):
 
 			time.sleep(10) # wait for user to be created
 			
-			try:
-				sts_client = boto3_client(
+			sts_client = boto3_client(
 				"sts", aws_access_key_id=user_key.id, aws_secret_access_key=user_key.secret
 				)
+			
+			try:
 				deploy_account_id = sts_client.get_caller_identity()["Account"]
 				print(f"deploy_account_id: {deploy_account_id}")
-				role_arn = f"arn:aws:iam::{account_id}:role/ROLE-{deploy_account_id}"
+				assume_role_arn = f"arn:aws:iam::{account_id}:role/ROLE-{deploy_account_id}"
 
 				user.create_policy(
 					PolicyName=f"deploy-user-policy-{account_id}",
@@ -74,7 +75,7 @@ def lambda_handler(event, context):
 								{
 									"Effect": "Allow",
 									"Action": "sts:AssumeRole",
-									"Resource": role_arn,
+									"Resource": assume_role_arn,
 								}
 							],
 						}
@@ -92,8 +93,46 @@ def lambda_handler(event, context):
 				raise
 
 			time.sleep(10) # wait
+
+			# Assume role
+			try:
+				print("assuming role")
+				new_sts_client = boto3_client(
+				"sts", aws_access_key_id=user_key.id, aws_secret_access_key=user_key.secret
+				)
+				response = new_sts_client.assume_role(
+				RoleArn=assume_role_arn, RoleSessionName=f"AssumeRole{account_id}"
+        		)
+				temp_credentials = response["Credentials"]
+				print(f"Assumed role {assume_role_arn} and got temporary credentials.")
+			except ClientError as error:
+				print(
+					f"Couldn't assume role {assume_role_arn}. Here's why: "
+            		f"{error.response['Error']['Message']}"
+				)
+				raise
+
+			# list buckets
+			try:
+				print("listing buckets")
+				s3_resource = boto3.resource(
+       			"s3",
+        		aws_access_key_id=temp_credentials["AccessKeyId"],
+        		aws_secret_access_key=temp_credentials["SecretAccessKey"],
+        		aws_session_token=temp_credentials["SessionToken"],
+    			)
+				for bucket in s3_resource.buckets.all():
+					print(bucket.name)
+			except ClientError as error:
+				print(
+					f"Couldn't list buckets for the account. Here's why: "
+            		f"{error.response['Error']['Message']}"
+				)
+				raise
+
 		except Exception:
-			print("something went terribly wrong!")		
+			print("something went terribly wrong!")
+			
 
     # start Fargate task
 	if cluster_name != "":
@@ -129,7 +168,8 @@ def lambda_handler(event, context):
             'statusCode': 202,
             'body': json.dumps(str('Compute node creation initiated'))
         }
-	
+
+# TODO: fix error handling	
 def user_exists(user_name):
     try:
         iam_resource.get_user(UserName=user_name)
